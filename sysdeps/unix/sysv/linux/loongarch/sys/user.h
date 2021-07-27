1f602403fd3614a5df4b3b2560868a40a1f1bd17
caiyinyu <caiyinyu@loongson.cn>
2021-07-27 15:53:52 +0800

    LoongArch: Linux ABI
    Linux-specific code that is required for maintaining ABI compatibility.
    
            * sysdeps/loongarch/dl-irel.h: New file.
            * sysdeps/loongarch/libc-start.h: Likewise.
            * sysdeps/loongarch/nptl/bits/struct_rwlock.h: Likewise.
            * sysdeps/loongarch/nptl/pthread-offsets.h: Likewise.
            * sysdeps/loongarch/nptl/pthreaddef.h: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/bits/fcntl.h: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/bits/mman.h: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/bits/shm.h: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/bits/sigcontext.h: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/getcontext.S: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/makecontext.c: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/register-dump.h: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/setcontext.S: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/sigcontextinfo.h: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/swapcontext.S: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/sys/procfs.h: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/sys/ucontext.h: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/sys/user.h: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/ucontext-macros.h: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/ucontext_i.sym: Likewise.

---
+++ sysdeps/unix/sysv/linux/loongarch/sys/user.h
@@ -0,0 +1,31 @@
+/* Copyright (C) 2001-2021 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#ifndef _SYS_USER_H
+#define _SYS_USER_H	1
+
+#include <stdint.h>
+
+struct user_regs_struct
+{
+  uint64_t gpr[32];
+  uint64_t pc;
+  uint64_t badvaddr;
+  uint64_t reserved[11];
+};
+
+#endif	/* _SYS_USER_H */
