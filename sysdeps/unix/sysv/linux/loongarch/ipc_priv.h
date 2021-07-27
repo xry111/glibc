8ec5a813838c5ecf8d37b5477898b421547895fd
caiyinyu <caiyinyu@loongson.cn>
2021-07-27 15:49:52 +0800

    LoongArch: Linux Syscall Interface
    Contains the Linux system call interface, as well as the definitions of
    a handful of system calls.
    
            * sysdeps/loongarch/abort-instr.h: New file.
            * sysdeps/loongarch/hp-timing.h: Likewise.
            * sysdeps/loongarch/nptl/nptl-sysdep.S: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/arch-syscall.h: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/bits/signum.h: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/clone.S: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/ipc_priv.h: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/kernel-features.h: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/syscall.c: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/sysdep.S: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/sysdep.h: Likewise.
            * sysdeps/unix/sysv/linux/loongarch/vfork.S: Likewise.

---
+++ sysdeps/unix/sysv/linux/loongarch/ipc_priv.h
@@ -0,0 +1,22 @@
+/* Old SysV permission definition for Linux.  LoongArch version.
+   Copyright (C) 2021 Loongson Technology, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <sys/ipc.h> /* For __key_t  */
+
+#define __IPC_64 0x0
+#include <sysdeps/unix/sysv/linux/ipc_priv.h>
